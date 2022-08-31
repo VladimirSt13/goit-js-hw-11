bebc04ba63919239c5f2ed28e017e78b6e27bdf0
VladimirSt13 <Vladimir.Stognij@gmail.com>
2022-08-31 03:48:45 +0300

    add +60px to smooth scroll

--- src/index.js
+++ src/index.js
@@ -123,7 +123,7 @@ function smoothScroll() {
   console.log(gallery.firstElementChild);
 
   window.scrollBy({
-    top: cardHeight * 10,
+    top: cardHeight * 10 + 60,
     behavior: 'smooth',
   });
 }
